8d2520165808924802f1e12e97d199b6943fc82e
Hafise Nur Bacaksız <hafisenur.bacaksiz@gmail.com>
2021-06-26 22:02:24 +0300

    done

---
+++ src/components/Article.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+
+
+function Article({ title, date = "January 1, 1970", preview, minutes }) {
+  const emojis = makeEmojiList(minutes);
+
+  return (
+    <article>
+      <h3>{title}</h3>
+      <small>
+        {date} • {emojis} {minutes} min read
+      </small>
+      <p>{preview}</p>
+    </article>
+  );
+}
+
+export default Article;
